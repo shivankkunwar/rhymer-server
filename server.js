eb1d5de43ba6923fd16c9bacab035395b18246e6
shivank <shivankkunwar100@gmail.com>
2023-11-14 21:08:25 +0530

    working server provides Rhymes of provided headline

--- server.js
+++ server.js
@@ -1,28 +1,50 @@
+const dotenv = require('dotenv');
+const {Configuration, OpenAIApi} = require('openai');
+
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+dotenv.config();
 
+const OpenAI = require("openai");
+
+const openai = new OpenAI();
 app.post('/rhyme', async (req, res) => {
   const headline = req.body.headline;
 
   try {
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      prompt: `Translate this headline into a rhyme: ${headline}`,
-      temperature: 0.5,
-      max_tokens: 60
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
-      }
+    const response = await openai.completions.create({
+      model: 'gpt-3.5-turbo-instruct',
+      prompt:`turn this sentence to Rhyme like a poem like "sky is blue" to "sky is blue and so are you" and keep it concise and short and make sure its  rhyming atleast the last word: ${headline}`,
+      max_tokens: 400,
+    temperature: 0, 
+      
     });
 
-    const rhyme = response.data.choices[0].text.strip();
-    res.json({ rhyme });
+    
+    if (response.choices && response.choices.length > 0) {
+      
+      const rhyme = response.choices[0].text.trim();
+      
+      if (rhyme) {
+       
+        res.json({ rhyme });
+      } else {
+       
+        console.log('Empty completion received');
+        res.status(500).json({ error: 'Empty completion received' });
+      }
+    } else {
+      
+      console.log('No choices received');
+      res.status(500).json({ error: 'No choices received' });
+    }
   } catch (error) {
+    console.error('Error generating rhyme:', error.message);
     res.status(500).json({ error: 'Error generating rhyme' });
   }
 });
